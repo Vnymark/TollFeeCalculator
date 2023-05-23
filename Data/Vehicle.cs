eac459538642c53aec2490e277e979adaabaf751
nymark <22155337+Vnymark@users.noreply.github.com>
2023-05-23 21:54:08 +0200

    Refactored Vehicle interface to a single Vehicle class.

--- Data/Vehicle.cs
+++ Data/Vehicle.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TollFeeCalculator
+﻿namespace TollFeeCalculator
 {
-    public interface Vehicle
+    public class Vehicle
     {
-        string GetVehicleType();
+        public string Type {get; set;}
+        public int TollFee {get; set;}
+        public Vehicle(string type)
+        {
+            Type = type;
+            TollFee = 0;
+        }
+        
+        /* Public functions */
+        public Boolean IsTollFree() {
+            return (Type == "Motorbike" ||
+                    Type == "Emergency" ||
+                    Type == "Tractor" ||
+                    Type == "Diplomat" ||
+                    Type == "Foreign" ||
+                    Type == "Military");
+        }
     }
 }
