beafeed4dc60bf8fcef68d3383ea359590ea91c6
nymark <22155337+Vnymark@users.noreply.github.com>
2023-05-25 00:11:34 +0200

    Built some nice printout and added test data.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TollFeeCalculator;
+
+/* Test data */
+Vehicle vehicle = new Vehicle("Car");
+DateTime[] datetimes = new DateTime[5]{
+    new DateTime(2023, 5, 17, 07, 10, 00),
+    new DateTime(2023, 5, 17, 06, 40, 00),
+    new DateTime(2023, 5, 17, 09, 15, 00),
+    new DateTime(2023, 5, 17, 10, 05, 00),
+    new DateTime(2023, 5, 17, 16, 15, 00)
+};
+
+/* Calculates the toll fees for the vehicle */
+TollCalculator tollCalculator = new TollCalculator();
+string description = "Tollfee for a {0} on {1}:";
+string result = "{0}kr";
+Console.WriteLine(string.Format(description, vehicle.Type, datetimes[0].ToShortDateString()));
+Console.WriteLine(string.Format(result, tollCalculator.GetTotalTollFee(vehicle, datetimes)));
+
